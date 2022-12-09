39cdf7929528211c2e1b15195b711f70dc114abe
Paula Díaz <115898219+pabudd@users.noreply.github.com>
2022-12-09 18:48:12 -0300

    Add files via upload

---
+++ js/ejemplo.js
@@ -0,0 +1,6 @@
+
+
+function cambiarModo () {var cuerpoweb = document.body.header.head;
+cuerpoweb.classList.toggle("oscuro");
+}
+function cartel ()  {alert("Paula Diaz mail: diazpaulad@gmail.com")}
